dcd08ff5fd2df35e0a26f88b233ab8622b2e5aaf
valenta-tomas <tomas.valenta@idealine.cz>
2023-02-07 19:50:57 +0100

    edit

---
+++ src/TextInput.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { TextBox } from 'devextreme-react'
+
+export default class TextInput extends Component {
+  render() {
+    return (
+      <TextBox/>
+    )
+  }
+}
--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,12 +1,12 @@
-﻿import React from "react";
+﻿import 'devextreme/dist/css/dx.light.css';
+import React from "react";
 import TextBox from "devextreme-react/text-box";
 const Text = ({children,color,value,...rest}) =>{
     const style={
         color:color,
     }
     return(
-        <TextBox style={style} value={value}> { children }</TextBox>
-    
+        <TextBox style={style} placeholder={value} label={children}></TextBox>
     );
 }
 export default Text;
